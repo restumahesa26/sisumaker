33a87ac36cecbc6785d8392e8973e9b157baeb22
Mufti Restu Mahesa <mufti.restumahesa@gmail.com>
2022-06-07 14:14:59 +0700

    update sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -37,6 +37,18 @@
                 <div data-i18n="Analytics">Surat Keluar</div>
             </a>
         </li>
+        <li class="menu-item">
+            <a href="#" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-dock-top"></i>
+                <div data-i18n="Analytics">Undangan</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="#" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-dock-top"></i>
+                <div data-i18n="Analytics">Proposal</div>
+            </a>
+        </li>
         <li class="menu-item @if(Route::is('laporan.*')) active @endif">
             <a href="{{ route('laporan.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bxs-file-doc"></i>
